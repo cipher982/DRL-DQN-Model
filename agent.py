48c10d849a973816ea70b7e7a25448a5d5477599
David Rose <david010@gmail.com>
2018-12-06 14:26:46 -0500

    In-process writing out agent/model code

---
+++ agent.py
@@ -0,0 +1,88 @@
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+from model import QNetwork
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-4               # learning rate
+UPDATE_EVERY = 4        # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Agent():
+    """Interacts with and learns from the environment"""
+
+    def init(self, state_size, action_size, seed):
+        """
+        Initialize an Agent object
+
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            seed (int): random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_Size
+        self.seed = random.seed(seed)
+
+        # Q-Network - TD difference (target - local)
+        self.qnetwork_local = QNetwork(state_Size, action_size, seed).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        # Replay memory
+        self.memory = ReplayBuffer
+
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples"""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """ 
+        Initialize a ReplayBuffer object
+
+        Params 
+        ======
+            action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", 
+                                    field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+
+        def add(self, state, action, reward, next_state, done):
+            """Add a new experience to memory"""
+            e = self.experience(state, action, reward, next_state, done)
+            self.memory.append(e)
+
+        def sample(self):
+        """Randomly sample a batch of experiences from memory"""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(
+            np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(
+            np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(
+            np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack(
+            [e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack(
+            [e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones)
---
+++ main_v2.py
@@ -0,0 +1,4 @@
+env = gym.make('LunarLander-v2')
+env.seed(0)
+print('State shape: ', env.observation_space.shape)
+print('Number of actions: ', env.action_space.n)
