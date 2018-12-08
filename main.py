c7a34c04d7ed5e1fedc27bd5799a32c274c461ca
David Rose <david010@gmail.com>
2018-12-08 13:23:04 -0500

    Name changing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from unityagents import UnityEnvironment
 import numpy as np
+import gym
+import random
+import torch
+from unityagents import UnityEnvironment
+from dqn_agent import Agent
+from collections import deque
 
 env = UnityEnvironment(file_name='Banana.app')
-print('got env')
+#env.seed(0)
+print('Loaded env')
 
 # get the default brain
 brain_name = env.brain_names[0]
@@ -19,31 +25,26 @@ print('Number of agents:', len(env_info.agents))
 action_size = brain.vector_action_space_size
 print('Number of actions:', action_size)
 
-# examine the state space 
+# examine the state space
 state = env_info.vector_observations[0]
 print('States look like:\n', state)
 state_size = len(state)
 print('States have length:', state_size)
+seed=0
 
-env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+#env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
 state = env_info.vector_observations[0]            # get the current state
-score = 0                                          # initialize the score
+score = 0
 
+agent = Agent(state_size=state_size, action_size=action_size, seed=seed)
 
-while True:
-    action = np.random.randint(action_size)        # select an action
-    #action = 1
-    print(action)
-    env_info = env.step(action)[brain_name]        # send the action to the environment
-    next_state = env_info.vector_observations[0]   # get the next state
-    reward = env_info.rewards[0]                   # get the reward
-        
-    done = env_info.local_done[0]                  # see if episode has finished
-    score += reward                                # update the score
-    state = next_state                             # roll over the state to next time step
-    if done:                                       # exit loop if episode finished
+# watch an untrained agent
+state = env.reset()
+for j in range(20):
+    action = agent.act(state)
+    env.render()
+    state, reward, done, _ = env.step(action)
+    if done:
         break
-    
-print("Score: {}".format(score))
 
-#env.close()
+env.close()
